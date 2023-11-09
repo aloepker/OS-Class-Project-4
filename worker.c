ba9eb0e612f18cc7b672ba78557453e2a06311b8
aloepker <aloepker@gmail.com>
2023-11-08 18:17:10 -0600

    Finished worker.c code, ready to test once oss.c is finished. Getting there!

--- worker.c
+++ worker.c
@@ -10,27 +10,26 @@
 #include <sys/types.h>
 #include <string.h>
 #include <errno.h>
-#define SHMKEY 859048
-#define BUFF_SZ sizeof ( int )
 #define PERMS 0777
 typedef struct msgbuffer {
 	long mtype;
 	char strData[100];
 	int intData;
 } msgbuffer;
-// !!! add random number generator code: copy from oss.c  !!!
-
-//worker's goals:
-//do a loop:
-	//check for message - blocking wait is fine - the message will contain the time the worker can posibly run for
-	//pick option of "process" to run after message, the options are for how much of the time quantum should be used.
-	//reply to the message sent by parent with the option chosen, aka, how long it actulally ran for.
-	// if not terminated, it waits for the next message to be sent by oss
-
-// time that the process actually uses if not all is randomly generated, so add pid seeded generator function
-//process percentages are something like 90-95% for full time, 3-4% for io and 1-2% for termination
+//random number generator for worker's task:
+int randOption(){
+	srand(getpid()+1);
+	return rand()%100;
+}
+//random number generator for workers time quantum used:
+int randTime(int max){
+	srand(getpid()+1);
+	return 1+rand()%max;
+}
 
 int main(int argc, char** argv){
+	int rNum;
+	int timeUsed;
 	msgbuffer buf;
 	buf.mtype = 1;
 	int msqid = 0;
@@ -51,26 +50,34 @@ int main(int argc, char** argv){
 		int argNano = atoi(argv[2]);
 		int timeUp = 0;
 
-		//loop that checks the clock:
+		//loop that checks the clock time sent:
 		while(timeUp != 1){
-//loop pt1. check for message from parent w/possible time quantum:
+		//loop pt1. check for message from parent w/possible time quantum:
 			// message queue read:
 			if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) {
 				perror("failed to recieve message form parent");
 				exit(1);
 			}
-//loop pt2. after message is recieved, use logic to decide between the 3 options: full time run, partial io, and partial w/termination.
-
-//loop pt3. respond to parrent with info related to the first
+			//after messaage is recieved, use logic to decide between the 3 options: full time run, partial io, and partial w/termination.
+			rNum = 10; //randOption();   currently a test value while building
+			if ((rNum < 3)){
+			//terminate if < 3
+				timeUsed = -randTime(buf.intData);
+				printf("WORKER PID: %d PPID %d SysClockS: %d SysclockNano %d TermTimeS: %d TermTimeNano: %d --Terminating\n",getpid(),getppid(),cint[0], cint[1], timeoutSec, timeoutNano);
+				timeUp = 1;
+			}else if((rNum < 9)){
+			//io opperation if < 9
+				timeUsed = randTime(buf.intData);
+			}else{
+			//full time quantum used
+				timeUsed = buf.intData
+			}
 				//message back to parent:
 				buf.mtype = getppid();
-				buf.intData = 0;
-				strcpy(buf.strData,"Some message back to Parent from Child\n");
+				buf.intData = timeUsed;
 				if (msgsnd(msqid, &buf, sizeof(msgbuffer)-sizeof(long),0) == -1) {
 					perror("msgsnd to parent failed\n");
 					exit(1);
 				}
-				printf("WORKER PID: %d PPID %d SysClockS: %d SysclockNano %d TermTimeS: %d TermTimeNano: %d --Terminating\n",getpid(),getppid(),cint[0], cint[1], timeoutSec, timeoutNano);
-// end loop. if not terminated, loop back to wait for the next message from oss
 		}
 }
